73a33df4901a3869874231cf2c7370403756c9ab
MillenniumRain <nik210597@gmail.com>
2020-12-05 13:01:40 +0500

    Показ кода во view

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-console.log('hi');
-console.log('hi2');
-console.log('hi3');
+
+let fullscreen = document.querySelector('.view_code');
+fullscreen.addEventListener('click', function(event) {  
+    let web_site = document.querySelector('.web_site');  
+    if(this.classList.contains('active')) {
+        this.classList.remove('active');
+        web_site.innerHTML = web_site.innerHTML.replace('<plaintext>', '');
+    } else {
+        this.classList.add('active');        
+        web_site.innerHTML = web_site.innerHTML.replace(/            /gm, '');
+        web_site.innerHTML = `<plaintext>` + web_site.innerHTML;
+    }
+
+});
+
+let pin = document.querySelector('.pin');
+pin.addEventListener('click', function(event) {    
+    let web_site = document.querySelector('.web_site');
+    let page_control = document.querySelector('.page_control');
+
+    if(this.classList.contains('active')) {
+        this.classList.remove('active');
+
+    } else {
+        this.classList.add('active');        
+
+    }
+
+});
