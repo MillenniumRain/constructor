28a85b4876db8a53986b1359872c47ce7b396d29
MillenniumRain <nik210597@gmail.com>
2020-12-10 13:42:35 +0500

    Добавление единиц измерения к полям

--- functions.js
+++ functions.js
@@ -5,17 +5,47 @@ for (let i = 0; i < input_units.length; i++) {
     if(input_units[i].classList.length) new_block.classList.add(input_units[i].classList);
     new_block.classList.add('units');
     let input = input_units[i].outerHTML;
-
+    let symbol  = input_units[i].value.match(/\D+/) || "px";
     new_block.innerHTML = `
-    <input class="switch-units" id="switch-units${++input_counter}" name="switch-units${i}" type="radio" checked="checked"><label class="switch-left" for="switch-units${input_counter}">px</label>
+    <input class="switch-units" id="switch-units${++input_counter}" name="switch-units${i}" type="radio" ${symbol == "px"?'checked="checked"':''}><label class="switch-left" for="switch-units${input_counter}">px</label>
     ${input}
-    <input class="switch-units" id="switch-units${++input_counter}" name="switch-units${i}" type="radio"><label class="switch-right" for="switch-units${input_counter}">&nbsp;%</label>
+    <input class="switch-units" id="switch-units${++input_counter}" name="switch-units${i}" type="radio" ${symbol == "%"?'checked="checked"':''}><label class="switch-right" for="switch-units${input_counter}">%</label>
     `;
     input_units[i].replaceWith(new_block);
 }
+
+let switch_units = document.querySelectorAll('.units .switch-units');
+for (let i = 0; i < switch_units.length; i++) {
+    switch_units[i].addEventListener('change', function(){
+        let input = this.parentElement.querySelector('input[type="text"]');
+        let num  = input.value.match(/\d+/);
+        if (num) input.value = num + this.nextSibling.innerText;
+    });    
+}
+
+
+
+
+
+
+// functions
 function registerInputOnChange(object, event_on){
     for (let i = 0; i < object.length; i++) {   
-        object[i].addEventListener(event_on, function(){
+        object[i].addEventListener(event_on, function(event){            
+            if (this.tagName == 'INPUT'){
+                let checked_unit = this.parentElement.querySelector('input:checked').nextSibling.innerText;
+                let unit_reg = this.value.match(/\D+/);
+                let unit = unit_reg ? unit_reg[0] : '' || checked_unit;
+                let number = this.value.match(/\d+/)[0];
+                if ((unit != '%') && (unit != 'px')) {
+                    unit = unit.replace('%','');
+                    this.value = number + unit;
+                } else {
+                    str = this.value.replace(unit, '');
+                    this.value = number + unit;
+                    this.setSelectionRange(number.length, number.length);
+                }
+            }
             if (edit_checkbox.checked) {
                 let css_text = this.value;
                 let css = this.getAttribute('data-tags-settings');
